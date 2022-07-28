8ac07333299f49b585553ed1ce0bce1f9a2b37d0
mabrune <mabrune.inf@gmail.com>
2022-07-28 19:35:34 -0300

    Backend project created

---
+++ backend/src/main/java/com/mydomain/fullcrud/FullcrudApplication.java
@@ -0,0 +1,13 @@
+package com.mydomain.fullcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullcrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mydomain/fullcrud/FullcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydomain.fullcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
