f6a95f6579059e483e26e0b616bbf6b86ee1be98
mabrune <mabrune.inf@gmail.com>
2022-07-30 17:51:18 -0300

    Find Client by id, Exception handling

--- backend/src/main/java/com/mydomain/fullcrud/services/ClientService.java
+++ backend/src/main/java/com/mydomain/fullcrud/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.mydomain.fullcrud.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.mydomain.fullcrud.dto.ClientDTO;
 import com.mydomain.fullcrud.entities.Client;
 import com.mydomain.fullcrud.repositories.ClientRepository;
+import com.mydomain.fullcrud.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,5 +23,12 @@ public class ClientService {
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Resource not found! Id = " + id));
+		return new ClientDTO(entity);
 	}
 }
